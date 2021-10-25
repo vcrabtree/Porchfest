9c48f37b34ce08a78556899f48bca03c6e1a19df
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2021-10-25 16:08:16 -0400

    updated routes

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,14 @@ from app.models import *
 @app.route('/')
 @app.route('/index')
 def index():
-    # artists_list = Artist.query.all()
-    # return jsonify({"artists": artists_list})
-    return jsonify({ "name": "Post Malone", "id": 7297, "hometown": "Grapevine, TX", "about": "Malone was born on July 4, 1995 in Syracuse, New York and moved to Grapevine, Texas at the age of 10. He started playing guitar at the age of 14 because of popular video game Guitar Hero . He later auditioned for band Crowd the Empire in 2010 but was rejected after his guitar string broke during the audition.", "photo": "https://i.scdn.co/image/93fec27f9aac86526b9010e882037afbda4e3d5f", "twitter": "https://twitter.com/postmalone", "spotify": "https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60", "instagram": "https://www.instagram.com/postmalone/", "merch": "https://shop.postmalone.com" })
+    artists = Artist.query.all()
+    artists_list = []
+    for art in artists:
+        artists_list.append(art.to_dict())
+
+    print(artists_list)
+    return jsonify(artists_list)
+    # return jsonify({ "name": "Post Malone", "id": 7297, "hometown": "Grapevine, TX", "about": "Malone was born on July 4, 1995 in Syracuse, New York and moved to Grapevine, Texas at the age of 10. He started playing guitar at the age of 14 because of popular video game Guitar Hero . He later auditioned for band Crowd the Empire in 2010 but was rejected after his guitar string broke during the audition.", "photo": "https://i.scdn.co/image/93fec27f9aac86526b9010e882037afbda4e3d5f", "twitter": "https://twitter.com/postmalone", "spotify": "https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60", "instagram": "https://www.instagram.com/postmalone/", "merch": "https://shop.postmalone.com" })
 
 
 
