59e5dfd9d0c1f8c57db6ff726ea9c961c601f4a0
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2021-12-13 15:03:37 -0500

    increased photo_url length limit

--- app/models.py
+++ app/models.py
@@ -71,7 +71,7 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     hometown = db.Column(db.String(64), index=True)
     about = db.Column(db.String(255))
-    photo = db.Column(db.String(255))
+    photo = db.Column(db.String(510))
     website = db.Column(db.String(128))
     spotify = db.Column(db.String(128))
     youtube = db.Column(db.String(128))
